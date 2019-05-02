ec3085a8380682cdbd6c97a9226cf69dfbf653aa
Christian Tarabusi <ctarabusi@teufel.de>
2019-05-02 09:51:22 +0200

    initial commit

---
+++ wamplibrary/src/main/kotlin/com/raumfeld/wamplibrary/Connection.kt
@@ -0,0 +1,28 @@
+package com.raumfeld.wamplibrary
+
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
+
+class Connection(
+        private val incoming: ReceiveChannel<String>,
+        private val outgoing: SendChannel<String>
+) : CoroutineScope by CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher()) {
+
+    fun forEachMessage(exceptionHandler: (Throwable) -> Unit, action: suspend (String) -> Unit) =
+            launch {
+                incoming.consumeEach { message ->
+                    try {
+                        Log.d("WAMP", "message received: $message")
+                        outgoing.send(message)
+                    } catch (throwable: Throwable) {
+                        exceptionHandler(throwable)
+                    }
+                }
+            }
+}
